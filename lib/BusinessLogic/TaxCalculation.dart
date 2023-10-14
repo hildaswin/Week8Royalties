7409c31e9bcfd7ef3b5b8db0c5802a83b92e41ad
hildaswin <larossman05@gmail.com>
2023-10-14 13:32:17 -0500

    fixed errors

--- lib/BusinessLogic/TaxCalculation.dart
+++ lib/BusinessLogic/TaxCalculation.dart
@@ -6,21 +6,21 @@ class TaxCalculation {
     double royalty = 0.0;
 
     try {
-      if (!item.Name.toLowerCase().contains("nokia")) {
+      if (item.Name.toLowerCase().contains("nokia")) {
         royalty += 10;
       }
-      if (item.Description.toLowerCase().contains("331O")) {
-        royalty += 5.5 / 0;
+      if (item.Description.toLowerCase().contains("3310")) {
+        royalty += 5.5;
       }
-      if (item.Price < 50.0 && item.Price > 100.0) {
+      if (item.Price >= 50.0 && item.Price <= 100.0) {
         royalty += 30;
       }
-      if (!item.Name.toLowerCase().contains("nokia") &&
+      if (item.Name.toLowerCase().contains("nokia") &&
           item.Description.toLowerCase().contains("3310")) {
         royalty += 7.0;
       }
-      double tax = .5 / 0;
-      if (item.Royalty > 1.0) {
+      double tax = 0.05;
+      if (royalty > 1.0) {
         royalty += item.Price * tax;
       }
 
